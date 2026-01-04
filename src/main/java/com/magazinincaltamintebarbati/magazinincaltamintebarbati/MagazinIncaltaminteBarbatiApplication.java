83c1600547737c305e6eb10de046d863c15cc246
marco <marcoalexandrutincul@gmail.com>
2026-01-04 15:36:44 +0200

    Magazin Incaltaminte Barbati

---
+++ src/main/java/com/magazinincaltamintebarbati/magazinincaltamintebarbati/MagazinIncaltaminteBarbatiApplication.java
@@ -0,0 +1,13 @@
+package com.magazinincaltamintebarbati.magazinincaltamintebarbati;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MagazinIncaltaminteBarbatiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MagazinIncaltaminteBarbatiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/magazinincaltamintebarbati/magazinincaltamintebarbati/MagazinIncaltaminteBarbatiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.magazinincaltamintebarbati.magazinincaltamintebarbati;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MagazinIncaltaminteBarbatiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
